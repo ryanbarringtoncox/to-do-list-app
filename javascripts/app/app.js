b2b3b941b6b228f04a3d63cab606a0b077a16a1f
core.editor <ryanbarrington@gmail.com>
2013-03-05 13:21:54 -0500

    adds tab click functionality

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,21 +1,33 @@
 var main = function () {
-  //console.log("hello world!");
-  
+
+  //get json file of to-dos
   $.getJSON("all.json", function (todos) {
     var i;
     
-    /*for (i = 0; i < todos.length; i++) {
-      console.log(todos[i]);
-    };
-    */
+    //populate all body
     todos.forEach(function (todo) {
-      //console.log(todo.description);
+      
+      //to do description
       $("#all-body").append("<div class='to-do'>"+todo.description+"</div>");
+      
+      //to do categories
       todo.categories.forEach(function (category) {
-        //console.log("  " + category);
         $("#all-body").append("<div class='category'>"+category+"</div>");
       });
     });
+  });
+  
+  //when a tab is click...
+  $("#tabs > a").click(function() {
+    
+    //remove active classf from all bodies
+    $(".body").removeClass("active");
+    
+    //get class name
+    var this_class = $(this).attr("class");
+    
+    //make corresponding body div active
+    $("#"+this_class+"-body").addClass("active");
   });
 }
 
