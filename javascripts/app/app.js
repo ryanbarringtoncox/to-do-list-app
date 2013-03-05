bf3a4640ab444703fbacb252d3db25cd420475ed
core.editor <ryanbarrington@gmail.com>
2013-03-05 12:46:33 -0500

    prints to-dos to all tab

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -3,14 +3,17 @@ var main = function () {
   
   $.getJSON("all.json", function (todos) {
     var i;
-    for (i = 0; i < todos.length; i++) {
+    
+    /*for (i = 0; i < todos.length; i++) {
       console.log(todos[i]);
     };
-    
+    */
     todos.forEach(function (todo) {
-      console.log(todo.description);
+      //console.log(todo.description);
+      $("#all-body").append("<div class='to-do'>"+todo.description+"</div>");
       todo.categories.forEach(function (category) {
-        console.log("  " + category);
+        //console.log("  " + category);
+        $("#all-body").append("<div class='category'>"+category+"</div>");
       });
     });
   });
