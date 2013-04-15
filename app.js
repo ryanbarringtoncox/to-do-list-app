c013046d1066fbb82decac8d64b0cd007765d869
core.editor <ryanbarrington@gmail.com>
2013-04-15 16:57:20 -0400

    works mongoose into the mix

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 var http = require('http')
+  ,path = require('path')
+  ,mongoose = require('mongoose') 
   ,express = require('express')
-  ,path = require('path');
-  
-var app = express();
+  ,app = express();
 
+mongoose.connect('mongodb://localhost/development');
+
+var TodoSchema = {
+  "description": String,
+  "categories": [String]
+}
+
+var Todo = mongoose.model("Todo", TodoSchema);
+
+Todo.findOne({}, function (err, result) {
+    if (err !== null) {
+	console.log(err);
+    } else if (result === null) {
+	var p = new Todo({
+	    "description": "Have fun",
+	    "categories": ["work", "personal"]
+	});
+
+	p.save(function (err) {
+	    if (err !== null) {
+		console.log(err);
+	    }
+	});
+    }
+});
+  
 app.configure(function() {
    app.use(express.static(path.join(__dirname, "public")));
+});
+
+app.get("/todos.json", function(req, res) {
+  //var todoObject = {};
+  //todoObject.test = "Is this working";
+  
+  Todo.find({}, function(err, todos) {
+    if(err!=null) {
+      console.log(err);
+    } else {
+      res.json(todos);
+      console.log("request made");
+      console.log(todos);
+    }
+  })
 });
 
 http.createServer(app).listen(3000, function() {
