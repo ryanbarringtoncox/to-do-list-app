849d8b69601b2797994269bf1e643ff6eadf6bf8
core.editor <ryanbarrington@gmail.com>
2013-04-18 10:13:18 -0400

    abstracts controller

--- app.js
+++ app.js
@@ -1,46 +1,20 @@
 var http = require('http')
   ,path = require('path') 
   ,express = require('express')
-  ,app = express();
+  ,app = express()
+  ,todoController;
 
-Todo = require("./models/todo.js");
+//Todo = require("./models/todo.js");
+todoController = require('./controllers/todo_controller.js');
   
 app.configure(function() {
   app.use(express.static(path.join(__dirname, "public")));
   app.use(express.bodyParser());
 });
 
-app.get("/todos.json", function(req, res) {
-  //var todoObject = {};
-  //todoObject.test = "Is this working";
-  
-  Todo.find({}, function(err, todos) {
-    if(err!=null) {
-      console.log(err);
-    } else {
-      res.json(todos);
-      //console.log("request made");
-      //console.log(todos);
-    }
-  })
-});
-
-app.post("/todo/new", function(req, res) {
-  //console.log("post called");
-  var t = new Todo({
-    "description": req.body.description,
-    "categories": req.body.categories
-  });
-  
-  t.save(function(err, result) {
-    if (err!==null) {
-      console.log(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-
 http.createServer(app).listen(3000, function() {
   console.log("Server running on port 3000...")
 });
+
+app.get("/todos.json", todoController.list);
+app.post("/todo/new", todoController.create);
--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,5 +1,29 @@
-/*var Todo = require("../models/todo.js"),
-  ToDoController = {};
+var Todo = require("../models/todo.js"),
+  TodoController = {};
+  
+TodoController.list = function(req, res) {
+  Todo.find({}, function(err, todos) {
+    if(err!=null) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  }) 
+};
+
+TodoController.create = function(req, res) {
+  var t = new Todo({
+    "description": req.body.description,
+    "categories": req.body.categories
+  });
+  
+  t.save(function(err, result) {
+    if (err!==null) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  }); 
+};
   
 module.exports = TodoController;
-*/
